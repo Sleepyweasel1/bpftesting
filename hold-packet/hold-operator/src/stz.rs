2a460a21348ea58c9b77bbf9ea18daf2380a5509
kb <38758200+Sleepyweasel1@users.noreply.github.com>
2026-04-30 16:35:23 +0000

    stz work and cargo

--- hold-packet/hold-operator/src/stz.rs
+++ hold-packet/hold-operator/src/stz.rs
@@ -1,16 +1,27 @@
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::Time;
 use kube::CustomResource;
 use schemars::JsonSchema;
 use serde::{Serialize, Deserialize};
-use std::collections::BTreeMap;
-use k8s_openapi::apimachinery::pkg::util::intstr::IntOrString;
 
 
 /// Specification of desired alerting rule definitions for Prometheus.
 #[derive(CustomResource, Serialize, Deserialize, Clone, Debug, JsonSchema)]
 #[kube(group = "scale.sleepy.com", version = "v0", kind = "ScaleToZero", plural = "scaletozero")]
 #[kube(namespaced)]
+#[kube(status = "ScaleToZeroStatus")]
 pub struct ScaleToZeroSpec {
-    /// Content of Prometheus rule file
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub groups: Option<String>,
+    // an object that defines the reference objects to target (Deployment and related service)
+    pub target_ref: TargetRef,
+}
+#[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
+pub struct TargetRef {
+    pub deployment_name: String,
+    pub namespace: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, JsonSchema)]
+pub struct ScaleToZeroStatus {
+    pub replicas: i32,
+    checksum: String,
+    last_updated: Option<Time>,
 }
